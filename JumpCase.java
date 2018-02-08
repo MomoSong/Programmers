9a487a709caf73d205a7b1fc3b5e3cff15145484
401PC-D5\hong <johnmor78@gmail.com>
2018-02-08 14:53:46 +0900

    no message

--- JumpCase.java
+++ JumpCase.java
@@ -1,5 +1,19 @@
 package myJava;
 
 public class JumpCase {
+	
+	 public int jumpCase(int num) {
+	        int answer = 0;
+
+	        return answer;
+	    }
+
+	    public static void main(String[] args) {
+	        JumpCase c = new JumpCase();
+	        int testCase = 4;
+	        //아래는 테스트로 출력해 보기 위한 코드입니다.
+	        System.out.println(c.jumpCase(testCase));
+	    }
+	    
 
 }
